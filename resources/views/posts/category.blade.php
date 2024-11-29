2ba980594266487c3185988e4ef83267c0abeacb
Manuel-Ayusa <166891950+Manuel-Ayusa@users.noreply.github.com>
2024-11-29 14:10:26 -0300

    Crear componente para mostrar posts en una tarjeta

--- resources/views/posts/category.blade.php
+++ resources/views/posts/category.blade.php
@@ -6,27 +6,7 @@
             </h1>
 
             @foreach ($posts as $post)
-                <article class="mb-8 bg-white shadow-lg rounded-lg overflow-hidden">
-
-                    <img class="w-full h-72 object-cover object-center" src="http://api.codersfree.test/storage/{{$post->image->url}}" alt="">
-                    
-                    <div class="px-6 py-4">
-                        <h2 class="font-bold text-xl mb-2">
-                            <a href="{{route('posts.show', $post->id)}}">{{$post->name}}</a>
-                        </h2>
-                        <div class="text-gray-700 text-base">
-                            {{$post->stract}}
-                        </div>
-
-                        <div class="pt-4 pb-2">
-                            @foreach ($post->tags as $tag)
-                                <a href="#" class="inline-block bg-{{$tag->color}}-600 rounded-full px-3 py-1 text-sm text-white mr-2">{{$tag->name}}</a>
-                            @endforeach
-                        </div>
-
-                    </div>
-
-                </article>
+                <x-card-post :post="$post" />
             @endforeach
 
             <div class="mt-4">
