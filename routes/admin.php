c9f6815f615828ebefd95b3beef058b2a38a5c4b
Manuel-Ayusa <166891950+Manuel-Ayusa@users.noreply.github.com>
2025-01-14 15:51:56 -0300

    Crear rutas y controlador de usuarios para admin

--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\UserController;
 
 Route::get('', [HomeController::class, 'index'])->name('admin.home');
 
@@ -13,3 +14,5 @@ Route::resource('categories', CategoryController::class)->names('admin.categorie
 Route::resource('tags', TagController::class)->names('admin.tags');
 
 Route::resource('posts', PostController::class)->names('admin.posts');
+
+Route::resource('users', UserController::class)->names('admin.users');
